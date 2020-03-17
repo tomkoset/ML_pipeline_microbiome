e15cf547881fc4bd5535f01e4e440572f5873100
tomkoset <tomkoset@umich.edu>
2020-03-17 11:16:47 -0400

    Add input data for running models at family level, modify scripts to run models at family level

---
+++ code/slurm/L2_Logistic_Regression_d1_family.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+###############################
+#                             #
+#  1) Job Submission Options  #
+#                             #
+###############################
+
+# Name
+#SBATCH --job-name=L2_logit-d1_family
+
+# Resources
+# For MPI, increase ntasks-per-node
+# For multithreading, increase cpus-per-task
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4GB
+#SBATCH --time=5:00:00
+
+# Account
+#SBATCH --account=pschloss1
+#SBATCH --partition=standard
+
+# Logs
+#SBATCH --mail-user=tomkoset@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=%x-%j.out
+
+# Environment
+#SBATCH --export=ALL
+
+# Array
+#SBATCH --array=1-100
+
+# vector index starts at 0 so shift array by one
+
+seed=$(($SLURM_ARRAY_TASK_ID - 1))
+
+#####################
+#                   #
+#  2) Job Commands  #
+#                   #
+#####################
+
+# Making output dir for snakemake cluster logs
+mkdir -p logs/slurm/
+
+Rscript code/R/main.R --seed $seed --model L2_Logistic_Regression --data  test/data/classification_input_day1_data_family.csv --hyperparams test/data/hyperparams.csv --outcome dx
---
+++ code/slurm/combine_d0_family_output.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+###############################
+#                             #
+#  1) Job Submission Options  #
+#                             #
+###############################
+
+# Name
+#SBATCH --job-name=combine-d0-output
+
+# Resources
+# For MPI, increase ntasks-per-node
+# For multithreading, increase cpus-per-task
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1GB
+#SBATCH --time=5:00:00
+
+# Account
+#SBATCH --account=pschloss1
+#SBATCH --partition=standard
+
+# Logs
+#SBATCH --mail-user=tomkoset@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=%x-%j.out
+
+# Environment
+#SBATCH --export=ALL
+
+#####################
+#                   #
+#  2) Job Commands  #
+#                   #
+#####################
+
+# Making output dir for snakemake cluster logs
+mkdir -p logs/slurm/
+
+bash code/bash/family_d0_cat_csv_files.sh
